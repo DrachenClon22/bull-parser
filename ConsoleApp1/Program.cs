68daace3fdf5d028fbd26e9962baae23edb3f4a0
DrachenClon22 <drachenclon22@gmail.com>
2024-07-16 20:14:40 +0300

    добавил комментарий(важный)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@
         static async Task Main()
         {
             // Парсинг (пример)
+            // Плохой пример, лучше создавать таск, работа продолжительная (очень)
             Bull[]? result = await Parser.GetBullsAsync(new ConsolePrinter(20));
         }
     }
