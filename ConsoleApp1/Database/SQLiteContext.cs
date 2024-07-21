33666c287a5e033574d849296805c9771ed00d95
DrachenClon22 <drachenclon22@gmail.com>
2024-07-21 20:10:29 +0300

    Added sqlite context

---
+++ ConsoleApp1/Database/SQLiteContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp1.Database
+{
+    public partial class SQLiteContext : DbContext
+    {
+        public SQLiteContext()
+        {
+            Database.EnsureCreated();
+        }
+        public virtual DbSet<Bull> Bulls { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("Data Source=test.db");
+    }
+}
